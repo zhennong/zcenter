3a5d00530cd2643ce990d68910953d668eedef9c
wodrow <wodrow451611cv@gmail.com>
2016-06-12 18:00:02 +0800

    add members

---
+++ backend/modules/members/members.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\members;
+
+/**
+ * members module definition class
+ */
+class members extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\members\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
